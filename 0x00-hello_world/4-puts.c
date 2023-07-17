d485f9ee975efcd4394a0135467bd98135cf4a6a
Youssef jo <joahmedy777@gmail.com>
2023-07-17 15:06:48 -0700

    alias

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
-#include <stdio.c> 
+#include <stdio.c>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
