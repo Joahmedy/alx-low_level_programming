459e68563445564c568e6e3e7dad37b6f598ab3b
Youssef jo <joahmedy777@gmail.com>
2023-07-18 19:16:34 -0700

    y

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * printf_alphabet - utilizes on the _ptcher function to print
+ * 		thr alphabet a - z
+ */
+
+void print_alphabet(void)
+
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch ++)
+		_puchar(ch);
+	_puchar('\n');
