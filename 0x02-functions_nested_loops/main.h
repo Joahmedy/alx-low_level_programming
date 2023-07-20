ed45cd51ef81262077c3cac219fef0250ae2aeff
Youssef jo <joahmedy777@gmail.com>
2023-07-19 17:07:37 -0700

    jo

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,2 @@
-#include "main.h"
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+ 1 int _putchar(char c);
+ 2 void print_alphabet(void);
