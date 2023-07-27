57ec6ce4869e09f5f59f3e1ffc74c4a53a274cdb
Youssef jo <joahmedy777@gmail.com>
2023-07-27 19:24:28 +0300

    a

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <main.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+
+{
+
+  char a[] = "hello";
+char b[] = "world";
+
+strcat (a,b);
+    printf("%s",a);
+    return (0);
+}
+
