ef40d911a7ef2d0ff18275922523e8fcf24553ce
Youssef jo <joahmedy777@gmail.com>
2023-09-18 18:37:58 +0300

    beda

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - printf alphabets a to z 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+
+	int i;
+	int j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
