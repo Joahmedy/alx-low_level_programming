c690218042520c3ec406ef6c3847a4a749c4290b
Youssef jo <joahmedy777@gmail.com>
2023-10-19 22:09:24 +0300

    beda

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,4 @@
+#ifndef LISTS_H
 #define LISTS_H
 
 #include <stdio.h>
@@ -14,9 +15,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 
@@ -28,6 +29,5 @@ int _putchar(char c);
 int _strlen(char *s);
 list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
-
 
 #endif
